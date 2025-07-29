826d0aac32a229b9f62dd6a4b57fb474075c813d
matheus-nicolau <matheusnicolau.dev@gmail.com>
2025-07-28 20:52:05 -0400

    initial commit.

---
+++ src/test/java/matheus_nicolau/credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package matheus_nicolau.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
