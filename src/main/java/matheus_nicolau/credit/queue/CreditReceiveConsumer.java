068f511b15b9703f8c0feb2a718823a7829cc824
matheus-nicolau <matheusnicolau.dev@gmail.com>
2025-08-01 19:06:41 -0400

    feat: implements the consumer to queue service.

--- src/main/java/matheus_nicolau/credit/queue/CreditReceiveConsumer.java
+++ src/main/java/matheus_nicolau/credit/queue/CreditReceiveConsumer.java
@@ -1,5 +1,10 @@
 package matheus_nicolau.credit.queue;
 
+import com.google.gson.Gson;
+import matheus_nicolau.credit.dto.CreditDTO;
+import matheus_nicolau.credit.exceptions.ExceptionMessages;
+import matheus_nicolau.credit.service.CreditService;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -7,8 +12,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class CreditReceiveConsumer {
 
+    private final CreditService creditService;
+
+    public CreditReceiveConsumer(CreditService creditService) {
+        this.creditService = creditService;
+    }
+
     @RabbitListener(queues = "${mq.queues.credit-emission}")
-    public void receiveSolicitation(@Payload String message) {
-        System.out.println("Enviando mensagem para o RabbitMQ: " + message);
+    public void receiveSolicitation(@Payload String json) {
+        try {
+            Gson gson = new Gson();
+            CreditDTO credit = gson.fromJson(json, CreditDTO.class);
+            
+            if (null == credit){
+                throw new AmqpRejectAndDontRequeueException(ExceptionMessages.PAYLOAD_NOT_FOUND.message());
+            }
+            creditService.createCredit(credit);
+            
+        } catch (Exception e) {
+            throw new AmqpRejectAndDontRequeueException(ExceptionMessages.BAD_SOLICITATION.message());
+        }
+
     }
 }
--- src/main/java/matheus_nicolau/credit/service/CreditService.java
+++ src/main/java/matheus_nicolau/credit/service/CreditService.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import matheus_nicolau.credit.dto.CreditDTO;
 import matheus_nicolau.credit.entity.Credit;
 import matheus_nicolau.credit.exceptions.CreditAlreadyExistsException;
+import matheus_nicolau.credit.exceptions.ExceptionMessages;
 import matheus_nicolau.credit.parse.ParseCreditDTOToCredit;
 import matheus_nicolau.credit.parse.ParseCreditToCreditDTO;
 import matheus_nicolau.credit.repository.CreditRepository;
@@ -14,22 +15,22 @@ import java.util.List;
 @Service
 public class CreditService {
 
+    private final CreditRepository creditRepository;
     private final ParseCreditDTOToCredit parseCredit;
     private final ParseCreditToCreditDTO parseCreditDTO;
-    private final CreditRepository creditRepository;
 
-    public CreditService(ParseCreditDTOToCredit parseCredit, ParseCreditToCreditDTO parseCreditDTO,
-                         CreditRepository creditRepository) {
+    public CreditService(CreditRepository creditRepository, ParseCreditDTOToCredit parseCredit,
+                         ParseCreditToCreditDTO parseCreditDTO) {
+        this.creditRepository = creditRepository;
         this.parseCredit = parseCredit;
         this.parseCreditDTO = parseCreditDTO;
-        this.creditRepository = creditRepository;
     }
 
     @Transactional
     public void createCredit(CreditDTO credit) {
         Credit parse = parseCredit.parse(credit);
         creditRepository.findOneByName(credit.name()).ifPresent(creditOptional -> {
-            throw new CreditAlreadyExistsException("Credito já cadastrado");
+            throw new CreditAlreadyExistsException(ExceptionMessages.CREDIT_ALREADY_EXISTS.message());
         });
         creditRepository.save(parse);
     }
